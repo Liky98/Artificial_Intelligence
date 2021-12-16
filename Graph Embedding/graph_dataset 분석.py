526ddb253f1afd8dc00b96276b5c8a21e292b6f0
LeeKihoon <leekihoon1689@gmail.com>
2021-12-16 17:09:34 +0900

    211216

--- Graph Embedding/graph_dataset 분석.py
+++ Graph Embedding/graph_dataset 분석.py
@@ -1,3 +1,15 @@
 """  Cora, Pubmed, citeseer  """
 """ 분석 및 network X로 변환 """
 
+path = "D:/From_Github/planetoid-master/data"
+
+import torch
+from torch_geometric.data import data
+#%%
+edge_index = torch.tensor([[0, 1, 1, 2],
+                        [1, 0, 2, 1]], dtype=torch.long)
+#%%
+x = torch.tensor([-1], [0], [1], dtype=torch.float)
+data = data(x=x, edge_index=edge_index)
+
+#>>> Data(edge_index=[2, 4], x=[3, 1])
