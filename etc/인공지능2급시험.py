5560077ecedbce342233d0ae59eec60850c99c15
LeeKihoon <leekihoon1689@gmail.com>
2021-12-22 15:46:53 +0900

    인공지능활용분석2급 사전테스트

---
+++ etc/인공지능2급시험.py
@@ -0,0 +1,18 @@
+txt = '인공지능 실무 활용 능력'
+txt = txt.replace("인공지능", "인간하하")
+
+print(txt[0:4])
+#%%
+n=1
+m=10
+while n<m :
+    if n%3==0 :
+        break
+    print(n)
+    n+=1
+
+#%%
+print("hello '%'입니다.")
+#%%
+potato = "감자깡도 채소인가요?"
+print(potato.replace("감자깡","감자"))
