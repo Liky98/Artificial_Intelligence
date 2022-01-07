ee88c2e1f41159cb331fe3b0e38f7409d0bd3b0d
LeeKihoon <leekihoon1689@gmail.com>
2022-01-07 13:50:28 +0900

    최종 전처리 분할 데이터셋 제작

--- Graph Embedding/dblp/DBLP 1차 전처리.py
+++ Graph Embedding/dblp/DBLP 1차 전처리.py
@@ -1,15 +1,46 @@
+""" 필수 키값이 전부 존재하는 데이터만 남기는 코드"""
 import json
 import pandas as pd
 import gc
 import pickle
 import re
 
+path = "C:/Users/LeeKihoon/Desktop/새 폴더/split"
 count = 0
+find_list = ["_id", "title", "authors", "venue", "year", "keywords",
+             "fos", "references", "n_citation", "publisher", "abstract"]
 
 def processing() :
     global load_data
     global count
+    global del_list
 
+    while 1 :
+        for list in find_list :
+            if list not in load_data[count] :
+                del load_data[count][list]
+        count+=1
+
+#NumberInt값 대체하고 json 파일 열기
+for i in range(17) :
+    with open(path+str(i+1)+".json", 'r', encoding='utf-8') as f:
+        data = f.read()
+    fixed_data = re.sub(r"NumberInt\((\d+)\)", r"\1", data) #NumberInt(0) 변환하기
+    load_data = json.loads(fixed_data)
+    print("parse_json result: %s" % type(data))
+
+    try:
+        processing()
+    except :
+        print("전처리끝")
+
+    with open("처리완료 "+str(i+1)+"번째.json", 'w') as f:
+        json.dump(load_data, f, indent=4)
+
+# The End
+
+"""
+        
     while 1 :
         if "_id" not in load_data[count]:
             del load_data[count] #딕셔너리 자체를 삭제
@@ -58,23 +89,4 @@ def processing() :
         else :
             count +=1
             continue
-
-
-""" NumberInt값 대체하고 json 파일 열기 """
-
-path = "C:/Users/LeeKihoon/Desktop/새 폴더/split"
-
-for i in range(17) :
-    with open(path+str(i+5)+".json", 'r', encoding='utf-8') as f:
-        data = f.read()
-    fixed_data = re.sub(r"NumberInt\((\d+)\)", r"\1", data) #NumberInt(0) 변환하기
-    load_data = json.loads(fixed_data)
-    print("parse_json result: %s" % type(data))
-    try:
-        processing()
-    except :
-        print("전처리끝")
-
-    with open("처리완료 "+str(i+5)+"번째.json", 'w') as f:
-        json.dump(load_data, f, indent=4)
-
+"""
