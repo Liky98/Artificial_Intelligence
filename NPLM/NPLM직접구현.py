48dfb3063dd8d17c7f44b81b4bd609544374791c
LeeKihoon <leekihoon1689@gmail.com>
2022-01-20 14:32:50 +0900

    NPLM, NNLM Models 분석

--- NPLM/NPLM직접구현.py
+++ NPLM/NPLM직접구현.py
@@ -12,61 +12,23 @@ import torch.nn as nn
 import torch.optim as optim
 from keras.models import Sequential
 from keras.layers import Dense, Activation
+import torch
+import torch.nn as nn
+import torch.optim as optim
 
-"""
-class NNLM(nn.Module):
-    def __init__(self):
-        super(NNLM, self).__init__()
-        self.C = nn.Embedding(n_class, m)
-        self.H = nn.Linear(n_step * m, n_hidden, bias=False)    #히든레이어 가중치
-        self.d = nn.Parameter(torch.ones(n_hidden))         #Bias Term
-        self.U = nn.Linear(n_hidden, n_class, bias=False)
-        self.W = nn.Linear(n_step * m, n_class, bias=False) #가중치
-        self.b = nn.Parameter(torch.ones(n_class))          #Bias Term
+device = "CUDA" if torch.cuda.is_available() else "cpu"
+print(f"Using {device} device")
 
-    def forward(self, X):
-        X = self.C(X) # X : [batch_size, n_step, m]
-        X = X.view(-1, n_step * m) # [batch_size, n_step * m]
-        tanh = torch.tanh(self.d + self.H(X)) # [batch_size, n_hidden]
-        output = self.b + self.W(X) + self.U(tanh) # [batch_size, n_class]
-        # NPLM의 스코어 벡터 y = b + Wx + U*tanh(d+Hx)
-        return output
-"""
-### Tensorflow로
-C = tf.Variable(tf.random_normal_initializer([1]), name='C')
-model = Sequential()
-model.add(Dense(units=64), input_dim=28*28, activarion='relu')
-model.compile(loss='categorical_crossentropy',optimizer='sgd', metrics=['accuracy'])
+train_data = 'you need to know how to code'
 
-
-###
-
-def load_excel(path) :
-    return pd.read_csv(path)
-
-def load_data(path):
-    #데이터 불러오는 함수
-    with open(path, 'r', encoding='utf-8') as f:
-        data = f.read()
-    return data
-
-def score_vector():
-    # 스코어 벡터 제작
-    #y = b + Wx + U*tanh(d+Hx)
-    print()
-
-def n_gram(text):
-    #n-gram 구현
-    words = text.split()
-
-    for i in range(len(words) -1 ) :
-        print(words[i], words[i+1])
-
-    n_gram_zip = list(zip(words, words[1:]))
-    return n_gram_zip
-
-def lookup_table() :
-    print()
+def processing(text) :
+    # 중복을 제거한 단어들의 집합인 단어 집합 생성.
+    word_set = set(text.split())
+    # 단어 집합의 각 단어에 고유한 정수 맵핑.
+    vocab = {tkn: i+2 for i, tkn in enumerate(word_set)}
+    vocab['<unk>'] = 0
+    vocab['<pad>'] = 1
+    return vocab
 
 def make_batch():
     input_batch = []
@@ -82,15 +44,40 @@ def make_batch():
 
     return input_batch, target_batch
 
-excel = load_excel("Emotion.csv")
-text = excel["Text"] #21459,
-voca = list(set(text))# 21405,
+class NNLM(nn.Module):
+    def __init__(self):
+        super(NNLM, self).__init__()
+        self.Embedding = nn.Embedding(n_class, m) #embedding
+        self.Hidden01 = nn.Linear(n_step * m, n_hidden, bias=False)    #히든레이어 가중치
+        self.Ones = nn.Parameter(torch.ones(n_hidden))         #Ones
+        self.Hidden02 = nn.Linear(n_hidden, n_class, bias=False)
+        self.Hidden03 = nn.Linear(n_step * m, n_class, bias=False) #가중치
+        self.Bias = nn.Parameter(torch.ones(n_class))          #Bias Term
 
-tokenizer = Tokenizer()
+    def forward(self, X):
+        X = self.Embedding(X) # X : [batch_size, n_step, m]
+        X = X.view(-1, n_step * m) # [batch_size, n_step * m]
+        tanh = torch.tanh(self.d + self.Hidden01(X)) # [batch_size, n_hidden]
+        output = self.b + self.Hidden03(X) + self.Hidden02(tanh) # [batch_size, n_class]
+        # NPLM의 스코어 벡터 y = b + Wx + U*tanh(d+Hx)
+        return output
 
-for sentence in voca :
-    tokenizer.fit_on_texts([sentence])
-    print(f"단어는 {tokenizer.word_index}")
-    print(f"인코딩은 {tokenizer.texts_to_sequences([sentence])[0]}")
-    print(to_categorical(tokenizer.texts_to_sequences([sentence])[0]))
+if __name__ == '__main__' :
+    n_step = 2
+    m = 2
+    n_hidden = 2
 
+    excel = pd.read_csv("Emotion.csv")
+    text = excel["Text"] #21459,
+    sentenses = list(set(text))# 21405,
+
+    n_class = len(sentenses)  # number of Vocabulary
+
+    model = NNLM()
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    input_batch, target_batch = make_batch()
+    input_batch = torch.LongTensor(input_batch)
+    target_batch = torch.LongTensor(target_batch)
