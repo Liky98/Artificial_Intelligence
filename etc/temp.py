504a1955312674bd9572c1905355eed77f9264d5
LeeKihoon <leekihoon1689@gmail.com>
2022-03-08 11:47:07 +0900

    temp

--- etc/temp.py
+++ etc/temp.py
@@ -2,4 +2,4 @@ if __name__ == '__main__':
     from torch_geometric.datasets import Yelp
     root = 'C:/Users/user/Desktop/Project/pytorch-geometric/datasets/'
     dataset = Yelp(root=root + 'Yelp')
-#temp
+#temp1
