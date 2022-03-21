3927f6fd6f7666b7efe26f112bfd24b551d23d0c
LeeKihoon <leekihoon1689@gmail.com>
2022-03-21 13:58:44 +0900

    Bert Chat Re

--- NLP/BERT/BERT 챗봇.py
+++ NLP/BERT/BERT 챗봇.py
@@ -29,4 +29,4 @@ def return_similar_answer(input):
     train_data['score'] = train_data.apply(lambda x: cos_sim(x['embedding'], embedding), axis=1)
 
 #%%
-print(return_similar_answer('안녕하세요'))
+print(return_similar_answer('안녕하세요'))#
